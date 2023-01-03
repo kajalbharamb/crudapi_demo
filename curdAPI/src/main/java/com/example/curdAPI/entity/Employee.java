88a5d270609c6f6602ecbbb25dca7b395ffcca8e
kajalbharamb <89125270+kajalbharamb@users.noreply.github.com>
2023-01-03 11:04:23 +0530

    first commit

---
+++ curdAPI/src/main/java/com/example/curdAPI/entity/Employee.java
@@ -0,0 +1,13 @@
+package com.example.curdAPI.entity;
+import lambok.Data;
+
+
+@Data
+public class Employee {
+    private int employeeid;
+    private String ename;
+    private double salary;
+
+
+}
+
---
+++ curdAPI/src/test/java/com/example/curdAPI/CurdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curdAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
